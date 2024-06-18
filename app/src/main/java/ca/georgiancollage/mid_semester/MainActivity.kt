b384d02dfb6861c4b0304d44bab445d48c17dba1
Vishal Chavda <angelvish1698@gmail.com>
2024-06-18 15:31:38 -0400

    Midterm Exam Completed

---
+++ app/src/main/java/ca/georgiancollage/mid_semester/MainActivity.kt
@@ -0,0 +1,30 @@
+package ca.georgiancollage.mid_semester
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    /**
+     * MainActivity.kt
+     * Author: Vishal Chavda
+     * Student ID: 200520835
+     * Date Last Modified: 18-06-2024
+     * Description: BMI Calcultor
+     *
+     * Version: 1.0
+     */
+}
